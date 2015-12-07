c2bdb189b128333aeb7096e6b3467988ffc436db
tyrox <macbookproretina@Byron.local>
2015-12-07 04:39:58 -0600

    Formulario para correos

---
+++ resources/views/mail/form/mail.blade.php
@@ -0,0 +1,8 @@
+
+		<div class="col s12 m6 l6 ">        
+            {!!Form::text('email',null,['placeholder' => 'Email'])!!}
+            {!!Form::text('subject',null,['placeholder' => 'Asunto'])!!}
+        </div>
+        <div class="">
+            {!!Form::textarea('content',null,['placeholder' => 'Mensaje'])!!}
+        </div class="">
